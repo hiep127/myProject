3c79ba6ef3630af2b110b94c91c429086cac55f3
hiep127 <duyhiep127198@gmail.com>
2020-12-05 17:33:00 +0700

    adding folder

---
+++ catkin_ws/build/gaitech_edu/CMakeFiles/free_space_navigation_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/free_space_navigation_node.dir/src/turtlebot/navigation/free_space_navigation/free_space_navigation.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/gaitech_edu/free_space_navigation_node.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/gaitech_edu/free_space_navigation_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/free_space_navigation_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/gaitech_edu/CMakeFiles/voice_teleop_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voice_teleop_node.dir/src/turtlebot/voice_teleop/voice_teleop.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/gaitech_edu/voice_teleop_node.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/gaitech_edu/voice_teleop_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voice_teleop_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
